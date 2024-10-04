0334437ef612a2d9f0a3ba131449472b9a7977a1
hailey-hy <lillyine.hy@gmail.com>
2024-10-04 18:05:05 +0900

    feat: create react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import InfiniteScroll from "./components/InfiniteScroll";
+
+function App() {
+  return (
+    <div className="App">
+      <InfiniteScroll />
+    </div>
+  );
+}
+
+export default App;
